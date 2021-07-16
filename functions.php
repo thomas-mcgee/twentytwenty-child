1868c9e8fc8c75eb280555b74281f9e2a6dcbe57
Thomas McGee <hello@thomasmcgee.me>
2021-07-16 20:46:22 +0000

    Team Members functionality.

--- functions.php
+++ functions.php
@@ -8,18 +8,31 @@
 function my_theme_enqueue_styles() {
 	
 	$parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+	
 	$theme = wp_get_theme();
+	
 	wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
 		array(),  // if the parent theme code has a dependency, copy it to here
 		$theme->parent()->get('Version')
 	);
+	
 	wp_enqueue_style( 'child-style', get_stylesheet_uri(),
 		array( $parenthandle ),
-		$theme->get('Version') // this only works if you have Version in the style header
+		$theme->get('Version') . time() // this only works if you have Version in the style header
 	);
 	
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+/*--------------------------------------------------------------
+# Custom head items.
+--------------------------------------------------------------*/
+function hfm_wp_head() { ?>
+	
+	<script src="https://kit.fontawesome.com/be5334ee40.js" crossorigin="anonymous"></script>
+	
+<?php }
+add_action( 'wp_head', 'hfm_wp_head' );
 
 /*--------------------------------------------------------------
 # Custom image sizes.
--- inc/acf.php
+++ inc/acf.php
@@ -10,16 +10,51 @@
 --------------------------------------------------------------*/
 function hfm_team_members_content() {
 	
-	 ?>
+	$team_members = get_field( 'team_members' );
+	
+	/* echo '<pre>';
+	print_r( get_field( 'team_members' ) );
+	echo '</pre>'; */ ?>
 	
 	<div class="grid-container">
 		
-		<div class="grid-25 tablet-grid-50">
-			
-			Here is where it will go . . . 
-			
-		</div>
+		<?php if( have_rows( 'team_members' ) ) :
 		
+			// Loop through rows.
+			while( have_rows( 'team_members' ) ) : the_row();
+				
+				$avatar = wp_get_attachment_image_src( get_sub_field( 'avatar' ), 'large_avatar' ); ?>
+				
+				<div class="grid-25 tablet-grid-50">
+				
+					<div class="team-member">
+						<p>
+							<img src="<?php echo $avatar[0]; ?>" />
+						</p>
+						<h3>
+							<?php the_sub_field( 'name' ); ?>
+						</h3>
+						<p>
+							<?php the_sub_field( 'job_title' ); ?>
+						</p>
+						<?php if ( get_sub_field( 'website' ) ) : ?>
+						<p>
+							<small>
+								<a href="<?php the_sub_field( 'website' ); ?>" target="_blank">
+									<i class="fal fa-globe"></i>
+								</a>
+							</small>
+						</p>
+						<?php endif; ?>
+					</div>
+				
+				</div>
+				
+			<?php // End loop.
+			endwhile;
+		
+		endif; ?>
+
 	</div>
 	
 <?php }
